d627abf72941226d5bd1d9009d69cc59d834de8a
Henoire1 <emmanueldjoman40@gmail.com>
2026-02-17 09:56:20 +0000

    done

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const app = express();
+const { sequelize } = require('./models');
 const cors = require('cors');
 const fs = require('fs');
 const path = require('path');
